56cdb240e1159dd1790c2ac8f44a40c76c4ee7dc
karodgers <kaunda.rodgers@gmail.com>
2024-11-07 07:33:36 +0300

    update: removed the use of sort package, implemented bubble sort for time sorting

--- internal/list/time_sort.go
+++ internal/list/time_sort.go
@@ -1,12 +1,16 @@
 package list
 
-import (
-	"sort"
-)
-
 // SortByTime sorts files by modification time (newest first)
+// Uses bubble sort algorithm
 func SortByTime(files []FileInfo) {
-	sort.Slice(files, func(i, j int) bool {
-		return files[i].ModTime.After(files[j].ModTime)
-	})
+	n := len(files)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			// Compare adjacent elements
+			if files[j].ModTime.Before(files[j+1].ModTime) {
+				// Swap them if they are in the wrong order
+				files[j], files[j+1] = files[j+1], files[j]
+			}
+		}
+	}
 }
